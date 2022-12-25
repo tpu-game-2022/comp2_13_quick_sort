75dfd1567f98111d1cfee8b023451e7d911e48b4
Mocha0924 <108125975+Mocha0924@users.noreply.github.com>
2022-12-25 16:17:23 +0900

    コミットしました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -2,11 +2,31 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
+#include <stdlib.h> 
 
 // クイックソート(引数が不適切であればfalseを返す)
+int MyCompare(const void* a,const void* b)
+{
+	if (((item*) a)->key > ((item*) b)->key)
+	{
+		return 1;
+	}
+	if (((item*)a)->key < ((item*)b)->key)
+	{
+		return -1; 
+	}
+	else
+	{
+		return 0;
+	}
+}
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
-
-	return false;
+	if (begin == NULL || end == NULL || begin > end)
+		return false;
+	if (begin + 1 == end)
+		return true;
+	qsort(begin,end-begin, sizeof(item), MyCompare);
+	return true;
 }
