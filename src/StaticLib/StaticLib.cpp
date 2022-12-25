acbf30c4ac59d659c9c006dcb798b4f2d51dc925
saeki <maruponn256@gmail.com>
2022-12-25 23:13:07 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -2,11 +2,28 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
+#include <stdlib.h>
 
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
 
-	return false;
+	if (!begin || !end || end - begin < 0)return false;
+
+    int count = end - begin;
+
+	qsort(begin, count, sizeof(item), comparison_base);
+
+    return true;
 }
+
+int comparison_base(const void* front, const void* back)
+{
+	if(((item*)front)->key > ((item*)back)->key) return 1;
+	
+	if(((item*)front)->key < ((item*)back)->key) return -1;
+	
+	else return 0;
+}
+
