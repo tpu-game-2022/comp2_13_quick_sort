7f75733a102ac2a50ff4a5815700de07ef8dde63
Nyasa0405 <105965435+Nyasa0405@users.noreply.github.com>
2022-12-26 08:43:25 +0900

    テストが通るように修正しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,50 @@
 
 #include "../include/lib_func.h"
 
+void Swap_Item(item* begin, item* i, item* j)
+{
+	item Temp_Items;
+	Temp_Items = *i;
+	*i=*j;
+	*j= Temp_Items;
+}
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
-	// ToDo: クイックソートで並び替えてください
+	const int ElementCount = end - begin;
+	if (!begin || !end)return false;
+	if (end < begin)return false;
+	int i, j;
+	int pivot;//中央値
+	item* left = begin;//始まり
+	item* right = begin+ElementCount-1;//終端
+	i = left->key;
+	j = right->key;
 
-	return false;
+	pivot = (left->key + right->key) / 2;
+
+	while (1)
+	{
+		while (left->key < pivot)
+			left++;
+		while (right->key > pivot)
+			right--;
+		if (left>=right)break;
+
+		Swap_Item(begin, left, right);
+		left++;
+		right--;
+	}
+
+	if (left->key < i - 1)
+	{
+		quick_sort(begin ,left);
+	}
+	if (right->key > j + 1)
+	{
+		quick_sort(right,end);
+	}
+	return true;
 }
+
