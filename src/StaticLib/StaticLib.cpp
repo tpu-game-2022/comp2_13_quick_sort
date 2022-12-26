1a7e5bcd4c1ac4be19320cf306731cbbc8cda3a5
simulacru <g2127041@st.t-kougei.ac.jp>
2022-12-26 16:12:15 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -7,6 +7,34 @@
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
-
+	if(begin == NULL || end == NULL || end < begin)
 	return false;
+
+	int i, j, pivot;
+	int  element = end - begin; //要素数
+	item* left = begin;
+	item* right = begin + element + 1;
+	i = left->key;
+	j = right->key;
+	pivot = left->key;
+
+	do {
+		do { left++; } while (i < pivot);
+		do { right--; } while (pivot < j);
+		if (i < j)
+		{
+			item* i = left;
+			item* j = right;
+			item tmp = *i;
+			*i = *j;
+			*j = tmp;
+		}
+	} while (i < j);
+
+	if (i < j)
+	{
+		quick_sort(begin, left);
+		quick_sort(right, end);
+	}
+	return true;
 }
