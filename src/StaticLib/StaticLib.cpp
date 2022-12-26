5c302258a2da069f4fcb81fd02568455f923ad7e
MettoNao <nt7.00.harry@gmail.com>
2022-12-26 15:22:39 +0900

    テスト通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,44 @@
 
 #include "../include/lib_func.h"
 
+void swap (item *x, item *y) {
+  item temp;    // 値を一時保存する変数
+
+  temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+	if (begin == NULL || end == NULL) return false;
+    if (begin >= end)return false;
 
-	return false;
+    int count= end - begin;
+	item* pivot = begin + 1;
+    item* left = begin;
+    item* right = begin + count - 1;
+
+	while (begin->key < end->key)
+    {
+        while ((end->key >= pivot->key) && (begin < end)) end--;
+
+        while ((begin->key <= pivot->key) && (begin < end)) end++;
+
+        swap(left, right);
+        left++;
+        right--;
+    }
+
+    if (left > begin) {
+        if (!quick_sort(begin, left))return false;
+    }
+
+    if (right < end) {
+        if (!quick_sort(right, end))return false;
+    }
+
+	return true;
 }
