9dcc8538c57dbedb4397c7084b0f00795039a590
SWD9 <kawaren623@icloud.com>
2023-01-01 00:36:07 +0900

    遅れました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -2,11 +2,50 @@
 #include "Windows.h"                    // Windows API の機能定義
 
 #include "../include/lib_func.h"
+void swap(item* a, item* b) {
+    item copy = *a;
+    *a = *b;
+    *b = copy;
+}
+
+static item* search_front(int key, item* begin, const item* end)
+{
+	while (begin->key <= key && begin < end)begin++;
+	return begin;
+}
+
+static item* search_rear(int key, const item* begin,item* end)
+{
+	while (key <= end->key && begin < end)end--;
+	return end;
+}
+
+item* partition(item* begin,const item* end)
+{
+	int key = begin->key;
+	item* head = search_front(key, begin + 1, end);
+	item* tail = search_rear(key, begin,begin + (end - begin) - 1);
+
+	while (head < tail) {
+		swap(head, tail);
+		head = search_front(key, head + 1, end);
+		tail = search_rear(key, begin,tail - 1);
+	}
+
+	swap(tail, begin);
+	return tail;
+}
 
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+	if (begin == NULL || end == NULL || end < begin)return false;
+	if (end - begin <= 1)return true;
 
-	return false;
+	item* pivot = partition(begin,end);
+	quick_sort(begin, pivot);
+	quick_sort(begin + 1, end);
+
+	return true;
 }
