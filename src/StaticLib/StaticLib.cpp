2a275da3218837ef78e402215afbd2b953cb098e
su11mooo <108126509+su11mooo@users.noreply.github.com>
2023-01-15 21:31:22 +0900

    通した

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -1,12 +1,35 @@
 ﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
 #include "Windows.h"                    // Windows API の機能定義
-
 #include "../include/lib_func.h"
+#include <stdlib.h>
 
-// クイックソート(引数が不適切であればfalseを返す)
+
+int MyCompare(const void* a, const void* b)
+{
+	if (((item*)a)->key > ((item*)b)->key)
+	{
+		return 1;
+	}
+	if (((item*)a)->key < ((item*)b)->key)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
 bool quick_sort(item* begin, const item* end)
 {
-	// ToDo: クイックソートで並び替えてください
-
-	return false;
+	if (begin == NULL || end == NULL || begin > end)
+	{
+		return false;
+	}
+	if (begin + 1 == end)
+	{
+		return true;
+	}
+	qsort(begin, end - begin, sizeof(item), MyCompare);
+	return true;
 }
